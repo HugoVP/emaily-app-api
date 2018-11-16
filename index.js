fcb119684993f9fda9e61dabd7e8b4b928b61e63
HugoVP <dev@paleo.host>
2018-11-16 13:39:55 -0600

    Refactoring env vars configuration
    - Adding example env var file
    - Updating .gitignore and .dockerignore files to ignore .env files
    - Setting default application port to 5000

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.get('/test', (req, res) => {
   res.send('test');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on PORT: ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
 });
